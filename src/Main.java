230aae0fe41fac06f2ac219ed8166bb561c67bca
RAMONA SAUCINITANU <saucinitanu.ramona@yahoo.com>
2021-10-21 21:32:33 +0300

    Modified

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello my beautiful world!");
+        System.out.println("Ramona");
     }
 }
