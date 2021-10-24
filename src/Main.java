f5dd00eb4ab76888f34661a4fa6d6eeca3be9d67
RAMONA SAUCINITANU <saucinitanu.ramona@yahoo.com>
2021-10-24 17:46:30 +0300

    Task

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,7 @@ public class Main {
         System.out.println("Hello");
         System.out.println("Ramona");
  //     Tema 2
-        short myFirstNumber = 1;
-        int myInt = myFirstNumber + 3;
-        System.out.println(myInt);
- //
-
+        System.out.println(2 + 3);
 
 
 
