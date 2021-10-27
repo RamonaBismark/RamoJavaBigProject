675a0742285f165d45112d2d836a41bb0290f8fb
RAMONA SAUCINITANU <saucinitanu.ramona@yahoo.com>
2021-10-27 18:42:32 +0300

    Task

---
+++ src/Lab2.java
@@ -0,0 +1,20 @@
+public class Lab2 {
+    public static void main(String[] args) {
+        printMyName();
+    }
+// Tema 1
+
+    public static void printMyName() {
+        System.out.println("Hello \nRamona");
+    }
+
+
+}
+
+
+
+
+
+
+
+
