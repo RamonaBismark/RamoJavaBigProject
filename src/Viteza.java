9f6965959619b7c3d952ff4b3fedce8d158b5acf
RAMONA SAUCINITANU <saucinitanu.ramona@yahoo.com>
2021-10-31 14:55:10 +0200

    Task

---
+++ src/Viteza.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Viteza {
+
+    public static void main(String[] args) {
+
+        float distanta;
+        double timp;
+        Scanner scanner3 = new Scanner(System.in);
+
+        System.out.println();
+    }
+}
